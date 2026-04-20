1839ab82e76839c7ca4cf4875c6e96ff260c98bd
Swayam <swayamkumar226@gmail.com>
2026-04-20 20:05:41 +0000

    post method done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const fs = require('fs');
-const { json } = require('stream/consumers');
 
 
 app.get('/todos', function (req, res) {
@@ -13,6 +12,22 @@ app.get('/todos', function (req, res) {
         }
         const tasks = JSON.parse(data);
         res.json(tasks);
+    })
+})
+
+app.post('/todos', function (req, res) {
+    fs.readFile('todos.json', 'utf-8', function (err, data) {
+        if (err) {
+            res.status(404).json({ message: "can't read file" });
+            return;
+        }
+        const todosArray = JSON.parse(data);
+        let isCompleted = false;
+        const title = req.body.title;
+        const todo = { id: todosArray.length, title: title, isCompleted: isCompleted };
+        todosArray.push(todo);
+        fs.writeFileSync('todos.json', JSON.stringify(todosArray));
+        res.status(201).todo;
     })
 })
 
